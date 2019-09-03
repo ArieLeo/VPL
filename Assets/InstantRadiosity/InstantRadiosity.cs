879e9e4138637f1c841f7ec5f588d18149c4b9d9
海贼猎人 <dongch007@gmail.com>
2019-09-04 00:36:33 +0800

    update

--- Assets/InstantRadiosity/InstantRadiosity.cs
+++ Assets/InstantRadiosity/InstantRadiosity.cs
@@ -22,13 +22,26 @@ public class InstantRadiosity : MonoBehaviour
         {
             Vector3 dir = this.GenerateHemisphereDir(Vector3.down);
 
+            Color lightCol = Color.white;
+
             for (int j = 0; j < bounces; j++)
             {
                 if (Physics.Raycast(this.transform.position, dir, out raycastHit))
                 {
-                    this.CreateVirtualPointLight(raycastHit.point + raycastHit.normal*0.001f, Color.white, 1.0f/photonNum);
+                    MeshRenderer renderer = raycastHit.transform.GetComponent<MeshRenderer>();
+                    if(renderer != null)
+                    {
+                        lightCol *= renderer.sharedMaterial.color;
 
-                    dir = this.GenerateHemisphereDir(raycastHit.normal);
+                        this.CreateVirtualPointLight(raycastHit.point + raycastHit.normal * 0.001f, lightCol, 1.0f / photonNum);
+
+                        dir = this.GenerateHemisphereDir(raycastHit.normal);
+                    }
+                    else
+                    {
+                        Debug.LogError("Raycast a collider without renderer " + raycastHit.transform.name);
+                        break;
+                    }
                 }
                 else
                     break;
